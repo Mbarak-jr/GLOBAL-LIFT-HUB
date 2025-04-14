f25ff6feaa19158074f7f78c9480b7f693d608bc
Mbarak-jr <mohammedmbarak77@gmail.com>
2025-04-14 20:01:11 +0300

    updated vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,6 +16,14 @@ export default defineConfig({
   build: {
     outDir: 'dist', // Default for Vite, but explicit is good
     emptyOutDir: true, // Clears old build files before rebuilding
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit for large chunks
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'redux', 'react-redux', 'axios'], // Chunking some large dependencies into separate files
+        },
+      },
+    },
   },
   base: '/', // Keep '/' for root domain, or change if deploying to subpath
 });
